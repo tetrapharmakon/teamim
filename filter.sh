99c25ec9c1b136c4837fde2f3c36fc6a7a41ddf2
Fouche <tetrapharmakon@gmail.com>
2017-02-07 21:53:07 +0100

    idiosyncratically commented filter.sh

--- filter.sh
+++ filter.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-grep -o 
-# grep filters the group of punctuation symbols:
-# it is obviously easy to customize (adding the
-# Unicode symbols of a non-Latin alphabet)
-"[.,;:?\!]" 
-# filename here:
-$1 
-# pipes to tr (tr removes newlines and
-# gives output on a single string)
-| tr -d "\n" 
-# pipes to fold: outputs the punctuation in 40
-# chars per line. No reason to explain why 40.
-| fold -w40; 
-# saves the formatted output on a .punct file
-> $1.punct
+grep -o "[.,;:?\!]" $1 | tr -d "\n" | fold -w40 > $1.punct
+#      |             |       |           |           |
+#      |             |       |           |           |
+#      --- grep filters the group of punctuation symbols:
+#		   it is obviously easy to customize (adding the
+# 		   Unicode symbols of a non-Latin alphabet)  |
+#                    |       |           |           |
+# filename here:-----'       |           |           |
+#                            |           |           |
+# pipes to tr (tr removes newlines and   |           |
+# gives output on a single string)       |           |
+#                                        |           |
+# pipes to fold: outputs the punctuation in 40       |
+# chars per line. No reason to explain why 40.       |
+#                                                    |
+# saves the formatted output on a .punct file--------'
