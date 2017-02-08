6096991895ff31364422ca90ccb5603b22c7985c
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-02-08 02:45:23 +0100

    tweaked naming in filter v2

--- filter_v2.sh
+++ filter_v2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cat $1 | tr --delete --complement '[:punct:]' > $1.punc
+cat $1 | tr --delete --complement '[:punct:]' > $1.intpunct
 
 # NOTE: [:punct:] is a standard POSIX regexp character group
 # equivalent to [!"\#$%&'()*+,\-./:;<=>?@\[\\\]^_`{|}~]
