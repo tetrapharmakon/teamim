3f5ec99b36fe2420c41084d4c5e2ecc2f6c8ff01
Fouche <tetrapharmakon@gmail.com>
2017-02-22 13:50:21 +0100

    cleanup

--- v3/MAKE.sh
+++ v3/MAKE.sh
@@ -3,6 +3,6 @@
 basename=${1##*/}
 basename=${basename%.*}
 
-python parse.py --input ../texts/$1.txt --output ./data.angles
+python parse.py --input ../texts/$1.txt --output ./$1-data.angles
 gnuplot gnu.plot
 mv plot.pdf ${basename}.pdf
