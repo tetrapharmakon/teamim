4745226a43220acfc275215e3646eb0ba89a303a
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-02-22 20:34:26 +0100

    produced graphs

--- v4/draw_all.sh
+++ v4/draw_all.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-./parse.py babel --split="\s\s"
+./draw.py babel --split="\s\s"
+./draw.py bible --split="The.*?Book of"
+./draw.py cathedral
+./draw.py hegel
+./draw.py proust --split="CHAPITRE"
+./draw.py ulysses --split="-- I+ --"
+./draw.py wallace
